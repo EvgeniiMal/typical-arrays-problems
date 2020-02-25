e8ee3f4735bdd8d12e2681559bcd52dd3f0b714c
EvgeniiMal <eugenemal88@gmail.com>
2020-02-25 16:38:57 +0200

    feat: add solucions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
+exports.min = function min(array) {
+    if (array === undefined || array.length == 0) {
+        return 0;
+    }
+    let minValue = array[0];
+    array.forEach(value => {
+        if (minValue > value) {
+            minValue = value;
+        }
+    });
+    return minValue;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array === undefined || array.length == 0) {
+        return 0;
+    }
+    let maxValue = array[0];
+    array.forEach(value => {
+        if (maxValue < value) {
+            maxValue = value;
+        }
+    });
+    return maxValue;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array === undefined || array.length == 0) {
+        return 0;
+    }
+    const arraySum = array.reduce((prev, curr) => prev + curr);
+    return arraySum / array.length;
+};
